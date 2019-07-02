bbab97a239a030789f1ec21ff6d7e1a44f47c086
Ahmad <hamadagh777@gmail.com>
2019-07-02 20:08:11 +0200

    2nd of july

--- DOM/practice-DOM/main.js
+++ DOM/practice-DOM/main.js
@@ -1,11 +1,13 @@
 // 1
 
+
+  
 let containerSelector = document.getElementById("container");
 console.log(containerSelector);
 
 // 2
 
-let containerSelector2 = document.getElementById("container");
+let containerSelector2 = document.querySelector("#container");
 console.log(containerSelector2);
 
 // 3
@@ -16,11 +18,25 @@ console.log(listItemsSelector);
 // 4
 
 let thirdListItem = document.querySelector("#container > ol > li.third");
-console.log(thirdListItem);
+
 
 // 5
 
-containerSelector.textContent += "Hello";
+
+ containerSelector.textContent += "Hello";
 
 // 6
+
+let footer = document.querySelector(".footer");
+footer.classList.add("main");
+console.log(footer);
+
+// 7
+
+footer.classList.remove("main");
+console.log(footer);
+
+// 8
+
+
 
